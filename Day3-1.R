93bc3b667fc4cdac217fd4f3b68916d4056cb09f
hjchoi0207 <chjin95@gamil.com>
2020-08-26 17:08:13 +0900

    다른 폴더로 이동되었음

--- Day3-1.R
+++
@@ -1,8 +0,0 @@
-#install.packages("gapminder")
-library(gapminder)
-gapminder[gapminder$country == 'Korea, Rep.',]
-gapminder[,c('year', 'lifeExp', 'gdpPercap')]
-#gapminder[gapminder$country=="Korea, Rep." & gapminder$year>=1990,c("lifeExp","pop")]
-#gapminder[gapminder$country=="Korea, Dem. Rep." & gapminder$year>=1990,c("lifeExp","pop")]
-
-
